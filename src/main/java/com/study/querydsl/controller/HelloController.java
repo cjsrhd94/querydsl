c0b28af11aaa2dbf30fe5bb0416d782c4d08afe6
cjsrhd94 <cjsrhd94@gmail.com>
2022-01-13 11:39:28 +0900

    feat: make Basic Test Code

--- src/main/java/com/study/querydsl/controller/HelloController.java
+++ src/main/java/com/study/querydsl/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.study.querydsl;
+package com.study.querydsl.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
